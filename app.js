006198e259fd7cae6ef676284f3182ec23a09dff
shaban <146794146+sarzhh@users.noreply.github.com>
2025-11-07 12:49:46 +0000

    Add form validation logic for creating a category and updating it

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require ('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const path = require('path')
+const {body, validationResult} = require('express-validator')
 const indexRouter = require('./routes/indexRouter')
 const categoriesRouter = require('./routes/categoriesRouter')
 const productRouter = require('./routes/productRouter')
--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,6 +1,53 @@
 const { request } = require('express')
 const db = require('../db/queries')
-const upload = require('../utils/multerConfig')
+const {body, validationResult, matchedData} = require('express-validator')
+
+const alphaErr = 'category name should only contain letters'
+const lengthErr = 'category name should be between 5 and 20 characters'
+
+
+const validateCreateCategory = [
+     body('category_name')
+     .trim()
+     .isAlpha().withMessage(alphaErr)
+     .isLength({ min: 5, max: 10 }).withMessage(lengthErr),
+
+     body('category_image').custom((value, {req})=>{
+        const imageErr = 'An image file is required'
+        const typeErr = 'Only jpeg or png is alllowed'
+
+         if(!req.file){
+            throw new Error(imageErr)
+         }
+         const allowedTypes = ['image/jpeg', 'image/png']
+         if (!allowedTypes.includes(req.file.mimetype)){
+            throw new Error(typeErr)
+         }
+         return true;
+     })
+]
+
+const validateUpdateCategory = [
+     body('category_name')
+     .optional({ values: "falsy" })
+     .trim()
+     .isAlpha().withMessage(alphaErr)
+     .isLength({ min: 5, max: 10 }).withMessage(lengthErr),
+
+     body('category_image') 
+    .custom((value, {req})=>{
+        const typeErr = 'Only jpeg or png is alllowed'
+        
+        if(!req.file){
+            return true;
+        }
+         const allowedTypes = ['image/jpeg', 'image/png']
+         if (!allowedTypes.includes(req.file.mimetype)){
+            throw new Error(typeErr)
+         }
+         return true;
+     })
+]
 
 async function getAllCategories(req,res) {
     const categories = await db.getAllCategories()
@@ -11,14 +58,25 @@ async function getCreateCategory(req, res) {
     res.render('createCategory')
 }
 
-async function postCreateCategory(req,res){
-    const {category_name} = req.body
+const postCreateCategory = [
+    validateCreateCategory,
+
+    async (req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).render('createCategory', {errors: errors.array()})
+    }
+
+
+    const {category_name} = matchedData(req)
     const {filename} = req.file
     const category_image = `/images/${filename}`
 
     await db.createCategory(category_name, category_image)
     res.redirect('/categories')
 }
+]
+
 
 async function getUpdateCategory(req,res) {
     console.log(req.params.id)
@@ -27,14 +85,39 @@ async function getUpdateCategory(req,res) {
     res.render('updateCategory', {category})
 }
 
-async function postUpdateCategory(req,res) {
-    const {category_name} = req.body
-    const {filename} = req.file
-    const category_image = `/images/${filename}`
-    const id = req.params.id
-    await db.updateCategory(category_name, category_image, id)
+const postUpdateCategory = [
+    validateUpdateCategory,
+
+    async (req,res)=> {
+    const errors = validationResult(req)
+    const id = req.params.id;
+     const existingCategory = await db.getCategory(id)
+    if(!errors.isEmpty()){
+        const category = await db.getCategory(id)
+        return res.status(400).render('updateCategory',{category, errors: errors.array()})
+    }
+
+    
+    let {category_name} = matchedData(req)
+
+    if(!category_name){
+         category_name = existingCategory.category_name
+    }
+    
+    
+    let category_image
+    if(req.file){
+         category_image = `/images/${req.file.filename}`
+    }else{
+         category_image = existingCategory.category_image
+    }
+
+    await db.updateCategory(category_name, category_image , id)
      res.redirect('/categories')
 }
+]
+
+
 
 module.exports = {
     getAllCategories,
