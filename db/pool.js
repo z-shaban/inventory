5e6922f142b9d61bc3c0988664945ff4046abb51
shaban <146794146+sarzhh@users.noreply.github.com>
2025-11-14 20:57:20 +0000

    fix connection string in pool

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const {Pool} = require('pg')
 
 module.exports = new Pool({
-    connectionString : process.env.DB_URL
+   connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
